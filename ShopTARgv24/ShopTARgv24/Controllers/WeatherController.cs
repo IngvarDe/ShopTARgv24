d4d73e781c287d34a050ed5a4a1c9a7eaf2fa2a5
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-10-13 17:32:50 +0300

    AccuWeatherApi

---
+++ ShopTARgv24/ShopTARgv24/Controllers/WeatherController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARgv24.Core.ServiceInterface;
+
+namespace ShopTARgv24.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private readonly IWeatherForecastServices _weatherForecastServices;
+
+        public WeatherController
+            (
+                IWeatherForecastServices weatherForecastServices
+            )
+        {
+            _weatherForecastServices = weatherForecastServices;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //teha action SearchCity
+        [HttpPost]
+        public IActionResult SearchCity()
+        {
+            return View();
+        }
+    }
+}
