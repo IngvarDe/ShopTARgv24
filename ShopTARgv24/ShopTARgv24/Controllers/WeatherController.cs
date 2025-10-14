db36642736a4dd9f7ad4f22d46b7138a32e31372
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-10-14 17:44:40 +0300

    weatherRoot

--- ShopTARgv24/ShopTARgv24/Controllers/WeatherController.cs
+++ ShopTARgv24/ShopTARgv24/Controllers/WeatherController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARgv24.Core.Dto;
 using ShopTARgv24.Core.ServiceInterface;
+using ShopTARgv24.Models.Weather;
 
 namespace ShopTARgv24.Controllers
 {
@@ -22,8 +24,25 @@ namespace ShopTARgv24.Controllers
 
         //teha action SearchCity
         [HttpPost]
-        public IActionResult SearchCity()
+        public IActionResult SearchCity(AccuWeatherSearchViewModel model)
         {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("City", "Weather", new { city = model.CityName });
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult City(string city)
+        {
+            AccuLocationWeatherResultDto dto = new();
+            dto.CityName = city;
+
+
+            _weatherForecastServices.AccuWeatherResult(dto);
+
             return View();
         }
     }
