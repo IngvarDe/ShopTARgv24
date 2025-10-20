a209087b8c5ae03f57e3236e88f8839777c92eb4
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-10-20 16:32:39 +0300

    RootDto

--- ShopTARgv24/ShopTARgv24.ApplicationServices/Services/WeatherForecastServices.cs
+++ ShopTARgv24/ShopTARgv24.ApplicationServices/Services/WeatherForecastServices.cs
@@ -10,9 +10,9 @@ namespace ShopTARgv24.ApplicationServices.Services
 
         public async Task<AccuLocationWeatherResultDto> AccuWeatherResult(AccuLocationWeatherResultDto dto)
         {
+            //https://developer.accuweather.com/core-weather/text-search?lang=shell#city-search
 
-
-            string accuApiKey = "your_api";
+            string accuApiKey = "zpka_0c86f3fafa9147e58813fa06b647f221_9b9fd9d9";
             string baseUrl = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/";
 
             using (var httpClient = new HttpClient())
@@ -24,14 +24,20 @@ namespace ShopTARgv24.ApplicationServices.Services
                 if (response.IsSuccessStatusCode)
                 {
                     var jsonResponse = await response.Content.ReadAsStringAsync();
-                    var weatherData = JsonSerializer.Deserialize<AccuLocationWeatherResultDto>(jsonResponse);
-                    return weatherData;
+                    var weatherData = JsonSerializer.Deserialize<AccuLocationRootDto>(jsonResponse);
+                    //return weatherData;
+
+                    dto.LocalObservationDateTime = weatherData.LocalObservationDateTime;
+                    dto.Text = weatherData.WeatherText;
+                    dto.TempMetricValueUnit = weatherData.Temperature.Metric.Value;
                 }
                 else
                 {
                     // Handle error response
                     throw new Exception("Error fetching weather data from AccuWeather API");
                 }
+
+                return dto;
             }
         }
     }
