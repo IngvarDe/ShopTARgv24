5374620a2260f6596f900e0a9bc73194c5acefb4
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-12-15 16:05:37 +0200

    Login

--- ShopTARgv24/ShopTARgv24/Controllers/AccountsController.cs
+++ ShopTARgv24/ShopTARgv24/Controllers/AccountsController.cs
@@ -61,7 +61,7 @@ namespace ShopTARgv24.Controllers
                     newsignup.Subject = "CRUD registration";
                     newsignup.To = user.Email;
 
-                    if(_signInManager.IsSignedIn(User) && User.IsInRole("Admin"))
+                    if (_signInManager.IsSignedIn(User) && User.IsInRole("Admin"))
                     {
                         return RedirectToAction("ListUsers", "Administrations");
                     }
@@ -83,7 +83,7 @@ namespace ShopTARgv24.Controllers
                     return View("ConfirmEmailMessage");
                 }
 
-                foreach(var error in result.Errors)
+                foreach (var error in result.Errors)
                 {
                     ModelState.AddModelError("", error.Description);
                 }
@@ -115,7 +115,7 @@ namespace ShopTARgv24.Controllers
                         "ActedOn", $"{user.Email}",
                         "CreatedAccountData", $"{user.Email}\n{user.City}\n[password hidden]\n[password hidden]"
                         ];
-            if(result.Succeeded)
+            if (result.Succeeded)
             {
                 errordatas =
                         [
@@ -132,7 +132,7 @@ namespace ShopTARgv24.Controllers
             ViewBag.ErrorDatas = errordatas;
             ViewBag.ErrorTitle = "Email cannot be confirmed";
             ViewBag.ErrorMessage = $"The users email, with userid of {userId}, cannot be confirmed.";
-            return View("Error", new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+            return View("Error", new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
         [HttpGet]
@@ -140,6 +140,54 @@ namespace ShopTARgv24.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model, string? returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+
+                if (user != null && !user.EmailConfirmed &&
+                    (await _userManager.CheckPasswordAsync(user, model.Password)))
+                {
+                    ModelState.AddModelError(string.Empty, "Email not confirmed yet");
+                    return View(model);
+                }
+
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, true);
+
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+
+                if (result.IsLockedOut)
+                {
+                    return View("AccountLocked");
+                }
+
+                ModelState.AddModelError("", "Invalid Login Attempt");
+            }
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
