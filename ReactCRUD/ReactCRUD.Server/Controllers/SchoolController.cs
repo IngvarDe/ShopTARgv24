2551482111cf0fcdc5444b14321ebf6911495a5e
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2026-01-13 17:23:02 +0200

    ReactCRUDinit

---
+++ ReactCRUD/ReactCRUD.Server/Controllers/SchoolController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReactCRUD.Data;
+using ReactCRUD.Server.ViewModels;
+
+namespace ReactCRUD.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchoolController : ControllerBase
+    {
+        private readonly ReactCRUDContext _context;
+
+        public SchoolController
+            (
+                ReactCRUDContext context
+            )
+        {
+            _context = context;
+        }
+
+        [HttpGet(Name = "SchoolList")]
+        public IActionResult Index()
+        {
+            var result = _context.Schools
+                .Select(x => new SchoolListViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Address = x.Address,
+                    StudentCount = x.StudentCount,
+                });
+
+            return Ok(result);
+        }
+    }
+}
